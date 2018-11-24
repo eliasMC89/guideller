775a629f0d93a1e0007c56a5f3006685dd689750
Elias Miralles <elias.miralles@gmail.com>
2018-11-24 13:10:38 +0100

    CRUD trips with no activities in it yet

--- starter-code/middlewares/authMiddleware.js
+++ starter-code/middlewares/authMiddleware.js
@@ -19,14 +19,30 @@ authMiddleware.requireUser = (req, res, next) => {
   next();
 };
 
-authMiddleware.checkUser = (req, res, next) => {
-  // console.log(req.params.activityId);
+// if user is owner of the activity or trip
+authMiddleware.checkActivityUser = (req, res, next) => {
   const activityId = req.params.activityId;
   const { _id } = req.session.currentUser;
   User.findById(_id)
     .then((user) => {
       const userActivities = user.activities;
       if (userActivities.indexOf(activityId) < 0) {
+        return res.redirect('/');
+      }
+    })
+    .catch(next);
+
+  next();
+};
+
+// if user is owner of the activity or trip
+authMiddleware.checkTripUser = (req, res, next) => {
+  const tripId = req.params.tripId;
+  const { _id } = req.session.currentUser;
+  User.findById(_id)
+    .then((user) => {
+      const userTrips = user.trips;
+      if (userTrips.indexOf(tripId) < 0) {
         return res.redirect('/');
       }
     })
---
+++ starter-code/routes/trips.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const Trip = require('../models/trip');
+const authMiddleware = require('../middlewares/authMiddleware'); // Middleware
+// const formMiddleware = require('../middlewares/formMiddleware');
+
+/* GET trips page. */
+router.get('/', (req, res, next) => {
+  const { _id } = req.session.currentUser;
+  User.findById(_id)
+    .populate('trips')
+    .then((user) => {
+      res.render('trips/my-trips', { user });
+    })
+    .catch(next);
+});
+
+// router.get('/my', authMiddleware.requireUser, (req, res, next) => {
+//   const { _id } = req.session.currentUser;
+//   User.findById(_id)
+//     .populate('activities')
+//     .then((user) => {
+//       res.render('activities/my-activities', { user });
+//     })
+//     .catch(next);
+// });
+
+// Render the create trips form
+router.get('/create', authMiddleware.requireUser, (req, res, next) => {
+  res.render('trips/create-trip', { title: 'Trips' });
+});
+
+// Receive the trips post
+router.post('/', authMiddleware.requireUser, (req, res, next) => {
+  // to see the information from the post, we need the body of the request
+  const { name, location, budget } = req.body;
+  const { _id } = req.session.currentUser;
+  const newTrip = new Trip({ name, location, budget });
+  const updateUserPromise = User.findByIdAndUpdate(_id, { $push: { trips: newTrip._id } });
+  const saveTripPromise = newTrip.save();
+
+  Promise.all([updateUserPromise, saveTripPromise])
+    .then(() => {
+      res.redirect('/trips');
+    })
+    .catch(next);
+});
+
+router.get('/:tripId/edit', authMiddleware.requireUser, authMiddleware.checkTripUser, (req, res, next) => {
+  const tripId = req.params.tripId;
+  Trip.findById(tripId)
+    .then((trip) => {
+      res.render('trips/edit-trip', { trip });
+    })
+    .catch(next);
+});
+
+// U in CRUD
+router.post('/:tripId/edit', authMiddleware.requireUser, authMiddleware.checkTripUser, (req, res, next) => {
+  console.log('posted the edit!!');
+  const tripId = req.params.tripId;
+  const updatedTripInformation = req.body;
+  Trip.findByIdAndUpdate(tripId, { $set: updatedTripInformation })
+    .then(() => {
+      res.redirect('/trips');
+    })
+    .catch(next);
+});
+
+// D in CRUD
+router.post('/:tripId/delete', authMiddleware.requireUser, authMiddleware.checkTripUser, (req, res, next) => {
+  const tripId = req.params.tripId;
+  Trip.deleteOne({ _id: tripId })
+    .then(() => {
+      res.redirect('/trips');
+    })
+    .catch(next);
+});
+
+module.exports = router;
